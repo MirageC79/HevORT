9b7fc7c5135947ca0c276723f1e0925f9a33a2ad
Marvin Beym <Marvin.Beym@gmx.de>
2022-03-18 17:58:15 +0100

    Change download button to support normal links too

---
+++ docs/assets/plugins/hevort-button/script.js
@@ -0,0 +1,15 @@
+window.$docsify = window.$docsify || {};
+window.$docsify.plugins = (window.$docsify.plugins || []).concat((hook) => {
+    const downloadRegEx = /\[>(.+)<\]\((.+?)\)/gm;
+    const normalRegEx = /\[>>(.+)<<\]\((.+?)\)/gm;
+
+    hook.beforeEach(content => {
+        return content
+            .replace(normalRegEx, (string, buttonName, filePath) => {
+                return `<a class="hevort-btn" href="${filePath}" target="_blank" rel="noopener"><span class="hevort-btn-text">${buttonName}</span></a>`;
+            })
+            .replace(downloadRegEx, (string, buttonName, filePath) => {
+                return `<a class="hevort-btn" href="${filePath}" target="_blank" rel="noopener"><span class="hevort-btn-text">${buttonName}</span><i class="fa fa-download hevort-button-icon" aria-hidden="true"></i></a>`;
+            })
+    });
+});
